6156b778da541aac6cc283464f704f401a7873ea
Viet Nguyen <viet.nguyen182@gmail.com>
2015-07-02 09:02:23 -0400

    Finish ListZipper

--- src/Course/ListZipper.hs
+++ src/Course/ListZipper.hs
@@ -659,8 +659,8 @@ instance Extend ListZipper where
 -- >>> id <<= (IsZ (zipper [2,1] 3 [4,5]))
 -- [[1] >2< [3,4,5],[] >1< [2,3,4,5]] >[2,1] >3< [4,5]< [[3,2,1] >4< [5],[4,3,2,1] >5< []]
 instance Extend MaybeListZipper where
-  (<<=) =
-    error "todo"
+  (<<=) f IsNotZ  = IsNotZ
+  (<<=) f (IsZ z) = IsZ (f . IsZ <<= z)
 
 -- | Implement the `Comonad` instance for `ListZipper`.
 -- This implementation returns the current focus of the zipper.
@@ -668,8 +668,8 @@ instance Extend MaybeListZipper where
 -- >>> copure (zipper [2,1] 3 [4,5])
 -- 3
 instance Comonad ListZipper where
-  copure =
-    error "todo"
+  copure (ListZipper _ c _ ) = c
+
 
 -- | Implement the `Traversable` instance for `ListZipper`.
 -- This implementation traverses a zipper while running some `Applicative` effect through the zipper.
@@ -681,8 +681,8 @@ instance Comonad ListZipper where
 -- >>> traverse id (zipper [Full 1, Full 2, Full 3] (Full 4) [Empty, Full 6, Full 7])
 -- Empty
 instance Traversable ListZipper where
-  traverse =
-    error "todo"
+  traverse f (ListZipper l c r) = (ListZipper . reverse) <$> traverse f (reverse l) <*> f c <*> traverse f r
+
 
 -- | Implement the `Traversable` instance for `MaybeListZipper`.
 --
@@ -694,8 +694,9 @@ instance Traversable ListZipper where
 -- >>> traverse id (IsZ (zipper [Full 1, Full 2, Full 3] (Full 4) [Full 5, Full 6, Full 7]))
 -- Full [1,2,3] >4< [5,6,7]
 instance Traversable MaybeListZipper where
-  traverse =
-    error "todo"
+  traverse f IsNotZ = pure IsNotZ
+  traverse f (IsZ z) = IsZ <$> traverse f z
+                       
 
 -----------------------
 -- SUPPORT LIBRARIES --
